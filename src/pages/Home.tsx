6937ca892c6adf1dc975b00052a46bb98ff92753
BeautyProBella <beautyprobella@gmail.com>
2025-12-07 23:22:00 -0500

    editing menu #4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -153,7 +153,7 @@ export default function Home() {
 
           {/* Hero image */}
           <Image
-            src="/hero_1.png"
+            src="/hero_22.png"
             alt="Patient talking with doctor using VeeVee"
             objectFit="cover"
             maxH="420px"
